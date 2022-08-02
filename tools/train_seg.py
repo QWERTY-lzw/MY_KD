33bbe420d88e4aa4290dd9137c3f0eec6efb9a54
Your Name <you@example.com>
2022-08-02 18:19:03 +0800

    train_seg

--- tools/train_seg.py
+++ tools/train_seg.py
@@ -13,11 +13,11 @@ from mmcv.runner import get_dist_info, init_dist
 from mmcv.utils import Config, DictAction, get_git_hash
 
 from mmseg import __version__
-from mmseg.apis import init_random_seed, set_random_seed, train_segmentor
+from mmseg_custom.apis import init_random_seed, set_random_seed, train_segmentor
 from mmseg.datasets import build_dataset
 from mmseg.models import build_segmentor
 from mmseg.utils import collect_env, get_root_logger
-from mmseg.distillation import build_distiller
+from mmseg_custom.distillation import build_distiller
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Train a segmentor')
