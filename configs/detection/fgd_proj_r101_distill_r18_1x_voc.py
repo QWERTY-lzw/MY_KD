be649024298b588cd2906416af096f3cea0dacb8
Your Name <you@example.com>
2022-08-02 16:18:39 +0800

    changes

---
+++ configs/detection/fgd_proj_r101_distill_r18_1x_voc.py
@@ -0,0 +1,125 @@
+_base_ = './retinanet_r18_fpn_1x_voc.py'
+# model settings
+find_unused_parameters=True
+alpha_fgd=1e-6
+distiller = dict(
+    type='DetectionDistiller',
+    teacher_pretrained = '/root/kd_fpn/work_dirs/retinanet_r101_fpn_1x_voc/latest.pth',
+    distill_cfg = [ dict(student_module = 'neck.fpn_convs.4.conv',
+                         teacher_module = 'neck.fpn_convs.4.conv',
+                         output_hook = True,
+                         methods=[dict(type='FGDPROLoss',
+                                       name='loss_mgd_fpn_4',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_fgd=alpha_fgd,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.3.conv',
+                         teacher_module = 'neck.fpn_convs.3.conv',
+                         output_hook = True,
+                         methods=[dict(type='FGDPROLoss',
+                                       name='loss_mgd_fpn_3',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_fgd=alpha_fgd,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.2.conv',
+                         teacher_module = 'neck.fpn_convs.2.conv',
+                         output_hook = True,
+                         methods=[dict(type='FGDPROLoss',
+                                       name='loss_mgd_fpn_2',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_fgd=alpha_fgd,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.1.conv',
+                         teacher_module = 'neck.fpn_convs.1.conv',
+                         output_hook = True,
+                         methods=[dict(type='FGDPROLoss',
+                                       name='loss_mgd_fpn_1',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_fgd=alpha_fgd,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.0.conv',
+                         teacher_module = 'neck.fpn_convs.0.conv',
+                         output_hook = True,
+                         methods=[dict(type='FGDPROLoss',
+                                       name='loss_mgd_fpn_0',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_fgd=alpha_fgd,
+                                       )
+                                ]
+                        ),
+
+                   ]
+    )
+
+student_cfg = 'configs/retinanet/retinanet_r18_fpn_1x_voc.py'
+teacher_cfg = 'configs/retinanet/retinanet_r101_fpn_1x_voc.py'
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(_delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
+dataset_type = 'VOCDataset'
+data_root = 'data/VOCdevkit/'
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', img_scale=(1000, 600), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1000, 600),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+data = dict(
+    samples_per_gpu=32,
+    workers_per_gpu=4,
+    train=dict(
+        type='RepeatDataset',
+        times=3,
+        dataset=dict(
+            type=dataset_type,
+            ann_file=[
+                data_root + 'VOC2007/ImageSets/Main/trainval.txt'
+            ],
+            img_prefix=[data_root + 'VOC2007/'],
+            pipeline=train_pipeline)),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'VOC2007/ImageSets/Main/test.txt',
+        img_prefix=data_root + 'VOC2007/',
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'VOC2007/ImageSets/Main/test.txt',
+        img_prefix=data_root + 'VOC2007/',
+        pipeline=test_pipeline))
+evaluation = dict(interval=1, metric='mAP')
+runner = dict(type='EpochBasedRunner', max_epochs=5)
+fp16 = dict(loss_scale=512.)
---
+++ configs/detection/simkd_rx101_distill_r50_2x_coco.py
@@ -0,0 +1,89 @@
+_base_ = './retinanet_r50_fpn_2x_coco.py'
+# model settings
+find_unused_parameters=True
+alpha_simkd=2e-5
+distiller = dict(
+    type='DetectionDistiller',
+    teacher_pretrained = 'https://download.openmmlab.com/mmdetection/v2.0/retinanet/retinanet_x101_64x4d_fpn_mstrain_3x_coco/retinanet_x101_64x4d_fpn_mstrain_3x_coco_20210719_051838-022c2187.pth',
+    distill_cfg = [ dict(student_module = 'neck.fpn_convs.4.conv',
+                         teacher_module = 'neck.fpn_convs.4.conv',
+                         output_hook = True,
+                         methods=[dict(type='SimKDLoss',
+                                       name='loss_simkd_fpn_4',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_simkd=alpha_simkd,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.3.conv',
+                         teacher_module = 'neck.fpn_convs.3.conv',
+                         output_hook = True,
+                         methods=[dict(type='SimKDLoss',
+                                       name='loss_simkd_fpn_3',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_simkd=alpha_simkd,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.2.conv',
+                         teacher_module = 'neck.fpn_convs.2.conv',
+                         output_hook = True,
+                         methods=[dict(type='SimKDLoss',
+                                       name='loss_simkd_fpn_2',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_simkd=alpha_simkd,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.1.conv',
+                         teacher_module = 'neck.fpn_convs.1.conv',
+                         output_hook = True,
+                         methods=[dict(type='SimKDLoss',
+                                       name='loss_simkd_fpn_1',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_simkd=alpha_simkd,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.0.conv',
+                         teacher_module = 'neck.fpn_convs.0.conv',
+                         output_hook = True,
+                         methods=[dict(type='SimKDLoss',
+                                       name='loss_simkd_fpn_0',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       alpha_simkd=alpha_simkd,
+                                       )
+                                ]
+                        ),
+
+                   ]
+    )
+
+student_cfg = 'configs/retinanet/retinanet_r50_fpn_2x_coco.py'
+teacher_cfg = 'configs/retinanet/retinanet_x101_64x4d_fpn_1x_coco.py'
+optimizer_config = dict(_delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
+'''
+fp16 = dict(loss_scale=512.)
+dataset_type = 'CocoDataset'
+data_root = 'data/coco/'
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instances_train2017.json',
+        img_prefix=data_root + 'images/train2017/'),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instances_val2017.json',
+        img_prefix=data_root + 'images/val2017/'),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instances_val2017.json',
+        img_prefix=data_root + 'images/val2017/'))
+'''
