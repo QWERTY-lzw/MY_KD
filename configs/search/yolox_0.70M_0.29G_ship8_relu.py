37705d7cab8e24eb1006546fe7e42fb66ed9b90e
Your Name <you@example.com>
2022-08-02 14:47:48 +0800

    remove old configs

--- configs/search/yolox_0.70M_0.29G_ship8_relu.py
+++
@@ -1,9 +0,0 @@
-_base_ = './yolox_0.70M_0.29G_ship8.py'
-
-act_type = 'ReLU'
-
-# model settings
-model = dict(
-    backbone=dict(act_cfg=dict(type=act_type)),
-    neck=dict(act_cfg=dict(type=act_type)),
-    bbox_head=dict(act_cfg=dict(type=act_type)))
