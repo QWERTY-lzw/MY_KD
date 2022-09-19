e9854cac14ca43d84cafbe619c3f67cc5e7e42ef
ubuntu <ubuntu@localhost.localdomain>
2022-09-19 17:05:48 +0800

    update

--- configs/detection/_base_/datasets/coco_detection.py
+++ configs/detection/_base_/datasets/coco_detection.py
@@ -1,14 +1,12 @@
-_base_ = './retinanet_r18_fpn_1x_coco.py'
 # dataset settings
-model = dict(bbox_head=dict(num_classes=20))
-dataset_type = 'VOCDataset'
-data_root = 'data/VOCdevkit/'
+dataset_type = 'CocoDataset'
+data_root = 'data/coco/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=(1000, 600), keep_ratio=True),
+    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
@@ -19,7 +17,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(1000, 600),
+        img_scale=(1333, 800),
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
@@ -31,30 +29,21 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=32,
-    workers_per_gpu=4,
+    samples_per_gpu=2,
+    workers_per_gpu=2,
     train=dict(
-        type='RepeatDataset',
-        times=3,
-        dataset=dict(
-            type=dataset_type,
-            ann_file=[
-                data_root + 'VOC2007/ImageSets/Main/trainval.txt'
-            ],
-            img_prefix=[data_root + 'VOC2007/'],
-            pipeline=train_pipeline)),
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instances_train2017.json',
+        img_prefix=data_root + 'train2017/',
+        pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
-        ann_file=data_root + 'VOC2007/ImageSets/Main/test.txt',
-        img_prefix=data_root + 'VOC2007/',
+        ann_file=data_root + 'annotations/instances_val2017.json',
+        img_prefix=data_root + 'val2017/',
         pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
-        ann_file=data_root + 'VOC2007/ImageSets/Main/test.txt',
-        img_prefix=data_root + 'VOC2007/',
+        ann_file=data_root + 'annotations/instances_val2017.json',
+        img_prefix=data_root + 'val2017/',
         pipeline=test_pipeline))
-evaluation = dict(interval=1, metric='mAP')
-runner = dict(type='EpochBasedRunner', max_epochs=5)
-optimizer = dict(type='SGD', lr=0.002, momentum=0.9, weight_decay=0.0001)
-optimizer_config=dict(_delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
-fp16 = dict(loss_scale=512.)
+evaluation = dict(interval=1, metric='bbox')
--- configs/detection/retinanet_r101_fpn_1x_voc.py
+++
@@ -1,6 +0,0 @@
-_base_ = './retinanet_r18_fpn_1x_voc.py'
-model = dict(
-    backbone=dict(
-        depth=101,
-        init_cfg=dict(type='Pretrained',checkpoint='torchvision://resnet101')),
-    neck=dict(in_channels=[256, 512, 1024, 2048]))
